9a4fc04a9d377b793bd3b85d5ea2bf921ebb5f6b
Jochem Breukers <jochemb864@gmail.com>
2025-05-12 08:43:51 +0200

    some queries that could be usefull

--- Dataset_maker.py
+++ Dataset_maker.py
@@ -1,21 +1,27 @@
 from Data_loader import data_loader
 
-def dataset2():
-    query = '''SELECT LSOA_code, count(Crime_type) as count
+def burglaries_month_LSOA():
+    """"
+    selects the amount of burglaries that happend that month in every LSOA
+    """
+    query = '''SELECT LSOA_code, month, count(Crime_type) as count
     FROM crimes
-    where Crime_type = 'burglary'
-    group by LSOA_code
+    where (Crime_type = 'burglary')
+    group by LSOA_code, month
     
     '''
     return data_loader(query)
 
-def query_Test1():
-    query = '''SELECT COUNT(DISTINCT LSOA_code) as count
-        FROM crimes
-        where Crime_type = 'burglary'
+def get_deprivation_score():
+    """
+    selects right deprivation score with LSOA code
+    """
+    query = '''SELECT FeatureCode as LSOA_code, value
+        FROM deprivation
+        where Measurement = 'Decile '
         '''
     return data_loader(query)
 
 
-print(dataset2())
+# print(burglaries_month_LSOA())
 # print(query_Test1())
